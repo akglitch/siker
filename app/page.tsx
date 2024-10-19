37c87f70276b86d0cf82f41321e08760a1f3e108
akglitch <mcaninakwa@gmail.com>
2024-10-19 16:55:06 -0100

    changed ui for  addassemblymember and government appointeeform.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import React from "react";
 import { Hero } from "./components/LandingPage";
+import AddAssemblyMemberForm from "./dashboard/AddAssemblyMemberForm";
 
 
 export default function Home() {
   return (
     
     <main className="mx-auto container">
+      <AddAssemblyMemberForm />
    <Hero />
 
     </main>
